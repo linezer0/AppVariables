ba8fbbf85dab419135818756005eb179d728f41c
Thomas Parker <thomasparker@MacBook-Pro-de-Thomas.local>
2014-11-05 08:05:33 +0100

    First commit

---
+++ Variables.playground/section-1.swift
@@ -0,0 +1,22 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var firstInteger:Int = 0, secondInteger:Int = 29
+var thirdDouble:Double = -1000844
+var x:Int = 5
+
+// I will be using this later to track the discounts at this departement store - it will change seasonaly
+var discountAtDepartementStore:Float = 0.3
+
+// First line 
+// Second line
+// UITableViewDataSource
+
+/*
+
+paragrap1 : Explanation 
+paragraph2 : Explanation
+
+*/
+
